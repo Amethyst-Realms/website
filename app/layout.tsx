d114ba40afc3d580a319638a253307def54de1b6
Lukas <69743171+quick007@users.noreply.github.com>
2022-12-24 20:42:20 -0800

    remove supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,22 @@
 import 'server-only'
 
 import "./globals.css";
-import SupabaseListener from '../components/supabase-listener'
-import createServerClient from '../lib/supabase-server'
-import { SupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '../lib/database.types';
+// import SupabaseListener from '../components/supabase-listener'
+// import createServerClient from '../lib/supabase-server'
+// import { SupabaseClient } from '@supabase/auth-helpers-nextjs';
+// import { Database } from '../lib/database.types';
 
 // do not cache this layout
 export const revalidate = 0
 
-export type TypedSupabaseClient = SupabaseClient<Database>;
+// export type TypedSupabaseClient = SupabaseClient<Database>;
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  const supabase = createServerClient()
+  // const supabase = createServerClient()
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession()
   
   return (
     <html lang="en">
